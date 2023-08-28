cc1bc1ac5a79678e5027f903204866268ba29069
root <mgiraudo@ies21.edu.ar>
2023-08-28 13:32:20 -0300

    Creacion del primer archivo

---
+++ frmMain.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace prySotoSP1
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCrearArch_Click(object sender, EventArgs e)
+        {
+            StreamWriter swCrearArchivo = new StreamWriter("Archivo1", false);
+
+            if (swCrearArchivo != null)
+                MessageBox.Show("Archivo creado");
+        
+        
+        }
+    }
+}
